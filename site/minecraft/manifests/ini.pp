d027ea40553edf7e9a11ff9e8f196b749f9c3bc7
stephenbrad515 <39060243+stephenbrad515@users.noreply.github.com>
2022-09-06 14:49:39 -0400

    Create ini.pp

---
+++ site/minecraft/manifests/ini.pp
@@ -0,0 +1,20 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+file {'/opt/minecraft/server.jar'}:
+  ensure => file,
+  source => 'https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+}
