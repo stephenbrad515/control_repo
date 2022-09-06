c3f5bc028fda021e23ceee2c98fee798be8fa40e
stephenbrad515 <39060243+stephenbrad515@users.noreply.github.com>
2022-09-06 16:07:28 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
   }
-file {'/opt/minecraft/server.jar'}:
+file {'/opt/minecraft/server.jar':
   ensure => file,
   source => 'https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar',
   }
