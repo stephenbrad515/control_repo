fc544eb45f80321ebe04b535970fce45cf44ccf0
stephenbrad515 <39060243+stephenbrad515@users.noreply.github.com>
2022-09-06 20:02:34 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,21 +1,21 @@
 class minecraft {
+  $url = 'https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar'
+  $install_dir = '/opt/minecraft'
+  $rpm_url = 'https://download.oracle.com/java/17/archive/jdk-17.0.4.1_linux-x64_bin.rpm'
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file{'/opt/minecraft/jdk-17.0.4.1_linux-x64_bin.rpm':
+  file {"${install_dir}/server.jar":
   ensure => file,
-  source => 'https://download.oracle.com/java/17/archive/jdk-17.0.4.1_linux-x64_bin.rpm',
-  }
-  file {'/opt/minecraft/server.jar':
-  ensure => file,
-  source => 'https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar',
+  source => $url,
+  before => Service['minecraft'],
   }
   package {'java':
     ensure => installed,
-    source => 'https://download.oracle.com/java/17/archive/jdk-17.0.4.1_linux-x64_bin.rpm',
+    source => $rpm_url,
     provider => 'yum',
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true',
   }
@@ -26,5 +26,6 @@ class minecraft {
   service {'minecraft':
     ensure => running,
     enable => true,
+    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
 }
